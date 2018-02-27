7e91ea6d9098529c1dade1191cdd747edbb23f35
jazzhong1 <jazzhong1@naver.com>
2018-02-27 14:57:30 +0900

    result

---
+++ src/com/iu/lang/math/Math_test1.java
@@ -0,0 +1,16 @@
+package com.iu.lang.math;
+
+public class Math_test1 {
+
+	public static void main(String[] args) {
+		
+		int a=(int)(Math.random()*10);
+		System.out.println(a);
+		
+		
+		
+		
+
+	}
+
+}
