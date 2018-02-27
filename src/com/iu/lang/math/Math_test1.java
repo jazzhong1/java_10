634b61d4714f37b906d4cb4f3475613ded4ddcd9
jazzhong1 <jazzhong1@naver.com>
2018-02-27 14:58:48 +0900

    result

--- src/com/iu/lang/math/Math_test1.java
+++ src/com/iu/lang/math/Math_test1.java
@@ -1,16 +1,17 @@
 package com.iu.lang.math;
 
+import java.util.*;
+
 public class Math_test1 {
 
 	public static void main(String[] args) {
 		
-		int a=(int)(Math.random()*10);
+		int a=(int)(Math.random()*10);	//별로 필요없다.
+										//
 		System.out.println(a);
 		
+		Date d = new Date(2011,12,12);
 		
-		
-		
-
 	}
 
 }
