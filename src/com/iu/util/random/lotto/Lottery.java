daf46396da0ece56cd17c874c967616235db5a7a
jazzhong1 <jazzhong1@naver.com>
2018-03-02 08:57:11 +0900

    result

--- src/com/iu/util/random/lotto/Lottery.java
+++ src/com/iu/util/random/lotto/Lottery.java
@@ -2,58 +2,48 @@ package com.iu.util.random.lotto;
 
 public class Lottery {
 	private Lotto lotto;
-	
+
 	public Lottery() {
-		lotto=new Lotto();
+		lotto = new Lotto();
 	}
-	
-	
-	
-	
-	public int[] check(int[] result){
+
+	public int[] check(int[] result) {
 		int num1[];
 		int num2[];
 		int num3[];
-		int count=0;
-		int count2=-1;
-	
-		num3=new int[LottoDataBase.lottoArray.length];
-		num1=result;
+		int count = 0;
+		int count2 = -1;
+
+		num3 = new int[LottoDataBase.lottoArray.length];
+		num1 = result;
 		for (int i = 0; i < LottoDataBase.lottoArray.length; i++) {
-			num2=LottoDataBase.lottoArray[i].getLotto();
+			num2 = LottoDataBase.lottoArray[i].getLotto();
 			for (int j = 0; j < num2.length; j++) {
 				for (int j2 = 0; j2 < num1.length; j2++) {
-					if(num1[j]==num2[j2]){
+					if (num1[j] == num2[j2]) {
 						count++;
 					}
 				}
-				count2++;
 
-				if(count==6){
-					count=1;
+				if (count == 6) {
+					count = 1;
+				} else if (count == 5) {
+					count = 2;
+				} else if (count == 4) {
+					count = 3;
+				} else if (count == 3) {
+					count = 4;
+				} else {
+					count = 0;
 				}
-				else if(count==5){
-					count=2;
-				}
-				else if(count==4){
-					count=3;
-				}
-				else if(count==3){
-					count=4;
-				}
-				else{
-					count=0;
-				}
-				
-				for (int j2 = count2; j2 <=count2; j2++) {
-					num3[j2]=count;
-					
-				}
+
 			}
-			
+
+			num3[i] = count;
 		}
+
 		return num3;
-		
+
 	}
 
 }
